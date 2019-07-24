611a35a27fe4e176f9c75efccb85c759766799de
spencerkingman-niche <skingman@niche.com>
2019-07-24 10:25:05 -0400

    Upd index.js

--- index.js
+++ index.js
@@ -5,44 +5,47 @@ const wikiData = require('./wiki-data.json');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const server = app.listen(3001, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
 
-  // highlight the query in the response text 
-  function _highlightQuery(query, text) {
-    const searchMask = new RegExp(query, "ig");
-    const highlightedText = text.replace(searchMask, function(match){
-      return `*${_matchCase(query, match)}*`
-    })
-    return highlightedText
-  }
-  
-  // Utility func for preserving case when highlighting query in response text
+// highlight the query in the response text 
+function _highlightQuery(query, text) {
+  const searchMask = new RegExp(query, "ig");
+  const highlightedText = text.replace(searchMask, function (match) {
+    return `*${_matchCase(query, match)}*`
+  })
+  return highlightedText
+}
+
+// Utility func for preserving case when highlighting query in response text
 function _matchCase(text, pattern) {
-    var result = '';
-    for(var i = 0; i < text.length; i++) {
-        var c = text.charAt(i);
-        var p = pattern.charCodeAt(i);
+  var result = '';
+  for (var i = 0; i < text.length; i++) {
+    var c = text.charAt(i);
+    var p = pattern.charCodeAt(i);
 
-        if(p >= 65 && p < 65 + 26) {
-            result += c.toUpperCase();
-        } else {
-            result += c.toLowerCase();
-        }
+    if (p >= 65 && p < 65 + 26) {
+      result += c.toUpperCase();
+    } else {
+      result += c.toLowerCase();
     }
-    return result;
+  }
+  return result;
 }
 
 // remove '---' and Highlight the heading
-function _formatHeading(text) {
+function _formatHeading(title, heading) {
+  const text = title === heading ? title : `${title}\t#${heading}`
   const formattedText = text.replace('---', '  ')
-  return `_${formattedText}_` 
+  return `_${formattedText}_`
 }
 
 app.post('/', (req, res) => {
-  // let text = req.body.text;
   const query = req.body.text.replace('-', '').toLowerCase();
   console.log('query:', query)
 
@@ -51,16 +54,14 @@ app.post('/', (req, res) => {
     const data = {
       "response_type": "ephemeral",
       "text": "*How to use /wiki*",
-      "attachments":[
-          {
-             "text":"To search the NicheInc GitHub Wikis, use `/wiki {YOUR_QUERY}`. For example, `/wiki kubernetes`.\n\nYou've already learned how to get help with `/wiki` or `/wiki help`."
-          }
-      ]
+      "attachments": [{
+        "text": "To search the NicheInc GitHub Wikis, use `/wiki {YOUR_QUERY}`. For example, `/wiki kubernetes`.\n\nYou've already learned how to get help with `/wiki` or `/wiki help`."
+      }]
     }
     res.json(data)
     return
   }
-  
+
   // If the user has typed a normal query
   let result = {};
   let resultText = ''
@@ -84,17 +85,30 @@ app.post('/', (req, res) => {
   //  res.send('U R DOIN IT WRONG. Enter a status code like 200!');
   //  return;
   // }
-  
+
+  // Convert results to array and sort by date descending.
+  const orderedResult = []
+  for (let id in result) {
+    orderedResult.push(result[id])
+  }
+  orderedResult.sort((itemA, itemB) => itemB.last_modified - itemA.last_modified)
+
   // prepare text ouput of response
-  for (var id in result) {
-    const item = result[id];
-    resultText += '-----------------------------------------\n\n';
-    resultText += _highlightQuery(query, _formatHeading(`${item.title} - ${item.heading}`)) + '\n';
-    resultText += `${item.heading_url}\n\n`;
-    resultText += _highlightQuery(query, item.content) + '\n\n';
+  for (let i = 0; i < orderedResult.length; i++) {
+    const item = orderedResult[i]
+    const date = new Date(item.last_modified)
+    const formattedDate = new Intl.DateTimeFormat('en-US', {
+      month: 'short',
+      year: 'numeric'
+    }).format(date)
+    resultText += `--------------------------------------------- ${i} of ${orderedResult.length}\n\n`;
+    resultText += _highlightQuery(query, _formatHeading(item.title, item.heading)) + '\n';
+    resultText += `${item.heading_url}\n`;
+    resultText += `_${item.last_author.substring(0, item.last_author.indexOf('<')-1)}_ in ${formattedDate}\n\n`;
+    resultText += `>${_highlightQuery(query, item.content)}\n\n`;
   }
 
-  // TODO: handle the no_text error
+  // handle the no_text error
   if (resultText === '') {
     resultText = `Sorry, *no results* were found for the query \`${req.body.text}\`.\n\n_Note: As of now, multi-word strings will only return results if the query words occur in the wiki in the same order they were entered._`
   }
